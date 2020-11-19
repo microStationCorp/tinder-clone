a0bab38482691d6a312c64768ff88fc603800f3e
microStationCorp <sujanmondal916@gmail.com>
2020-11-19 16:35:58 +0530

    2nd day tutorial

---
+++ src/component/header.js
@@ -0,0 +1,13 @@
+import "../css/Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+
+function Header() {
+  return (
+    <div>
+      <h1>it is header</h1>
+      <PersonIcon />
+    </div>
+  );
+}
+
+export default Header;
